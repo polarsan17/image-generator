2e9e85f641e5e8a35ccdf0e360d701ef6839cb74
polarsan17 <78031934+polarsan17@users.noreply.github.com>
2022-06-09 13:21:01 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  images: {
+    // should domains come from a configuration file? To discern, say, between dev,
+    // staging, and prod.
+    domains: ["geni-bucket.s3.us-west-2.amazonaws.com"],
+  },
+};
